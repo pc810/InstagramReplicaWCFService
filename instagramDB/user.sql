918dfef7d0c2ed6c2fdd604dd7e87a257acf7b38
Vyom <angerstick3@gmail.com>
2020-02-29 22:58:07 +0530

    User CRUD completed
    user sql file updated.
    Use your own connection string for crud operation

--- instagramDB/user.sql
+++ instagramDB/user.sql
@@ -2,7 +2,7 @@
     [userId]        INT           IDENTITY (1, 1) NOT NULL,
     [username]      NVARCHAR (50) NOT NULL,
     [email]         NVARCHAR (50) NOT NULL,
-    [dob]           DATE          NOT NULL,
+    [dob]           DATETIME      NOT NULL,
     [creation_date] DATETIME      NOT NULL,
     [password]      NVARCHAR (50) NOT NULL,
     PRIMARY KEY CLUSTERED ([userId] ASC)
